1c95003fc5dac245a2cefee04700665de03a2361
Craig Erskine <Craig>
2016-12-13 07:47:23 -0600

    initial

---
+++ extension.driver.php
@@ -0,0 +1,20 @@
+<?php
+	
+	Class extension_Textile extends Extension {
+	/*-------------------------------------------------------------------------
+		Definition:
+	-------------------------------------------------------------------------*/
+		
+		public function about() {
+			return array(
+				'name'			=> 'Textile',
+				'version'		=> '1.0.0',
+				'release-date'	=> '2016-12-12',
+				'author'		=> array(
+					'name'			=> 'Craig Erskine',
+					'website'		=> 'http://craigerskine.com/'
+				),
+				'description'	=> 'The superior text formatter.'
+			);
+		}
+	}
---
+++ text-formatters/formatter.textile.php
@@ -0,0 +1,35 @@
+<?php
+
+  Class formatterTextile extends TextFormatter{
+
+    private static $_parser;
+
+    public function about(){
+      return array(
+        'name' => 'Textile',
+        'version' => '1.0.0',
+        'release-date' => '2016-12-12',
+        'author' => array(
+          'name' => 'Craig Erskine',
+          'website' => 'http://craigerskine.com'
+        ),
+        'description' => 'The superior text formatter.'
+      );
+    }
+
+    public function run($string){
+      if(!self::$_parser){
+        if (!class_exists('Parser'))
+          include_once(EXTENSIONS . '/textile/lib/Parser.php');
+          include_once(EXTENSIONS . '/textile/lib/DataBag.php');
+          include_once(EXTENSIONS . '/textile/lib/Tag.php');
+        self::$_parser = new \Netcarver\Textile\Parser();
+      }
+
+      // Textile transformation
+      $result = stripslashes(self::$_parser->parse($string));
+
+      return $result;
+    }
+
+  }
