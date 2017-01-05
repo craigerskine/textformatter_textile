10ff6770568bd97ae66f05407954eb78ceca2bc4
Craig <craig@craigerskine.com>
2017-01-05 15:43:14 -0600

    Update formatter.textile.php

--- text-formatters/formatter.textile.php
+++ text-formatters/formatter.textile.php
@@ -6,23 +6,23 @@
 
     public function about() {
       return array(
-        'name' => 'Textile',
-        'version' => '1.0.0',
-        'release-date' => '2016-12-12',
-        'author' => array(
-          'name' => 'Craig Erskine',
-          'website' => 'http://craigerskine.com'
+        'name'          => 'Textformatter: Textile',
+        'version'       => '1.0.0',
+        'release-date'  => '2016-12-12',
+        'author'        => array(
+          'name'        => 'Craig Erskine',
+          'website'     => 'http://craigerskine.com'
           ),
-        'description' => 'The superior text formatter.'
+        'description'   => 'The superior text formatter.'
         );
     }
 
     public function run($string) {
       if(!self::$_parser) {
         if (!class_exists('Parser'))
-        include_once(EXTENSIONS . '/textile/lib/Parser.php');
-        include_once(EXTENSIONS . '/textile/lib/DataBag.php');
-        include_once(EXTENSIONS . '/textile/lib/Tag.php');
+        include_once(EXTENSIONS . '/textformatter_textile/lib/Parser.php');
+        include_once(EXTENSIONS . '/textformatter_textile/lib/DataBag.php');
+        include_once(EXTENSIONS . '/textformatter_textile/lib/Tag.php');
         self::$_parser = new \Netcarver\Textile\Parser();
       }
 
