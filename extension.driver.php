187a9c569c72c358d29300327873d1c08636040c
Craig <craig@craigerskine.com>
2017-01-05 15:31:51 -0600

    Update extension.driver.php

--- extension.driver.php
+++ extension.driver.php
@@ -1,20 +1,19 @@
 <?php
-	
-	Class extension_Textformatter_Textile extends Extension {
-	/*-------------------------------------------------------------------------
-		Definition:
-	-------------------------------------------------------------------------*/
-		
-		public function about() {
-			return array(
-				'name'			=> 'Textile',
-				'version'		=> '1.0.0',
-				'release-date'	=> '2016-12-12',
-				'author'		=> array(
-					'name'			=> 'Craig Erskine',
-					'website'		=> 'http://craigerskine.com/'
-				),
-				'description'	=> 'The superior text formatter.'
-			);
-		}
-	}
+  Class extension_Textformatter_Textile extends Extension {
+  /*-------------------------------------------------------------------------
+    Definition:
+  -------------------------------------------------------------------------*/
+
+    public function about() {
+      return array(
+        'name'      => 'Textile',
+        'version'   => '1.0.0',
+        'release-date'  => '2016-12-12',
+        'author'    => array(
+          'name'      => 'Craig Erskine',
+          'website'   => 'http://craigerskine.com/'
+        ),
+        'description' => 'The superior text formatter.'
+      );
+    }
+  }
